e3b489a18ef177728514d609f7f55117091148f2
272087260@qq.com <272087260@qq.com>
2018-11-27 10:38:19 +0800

    初始化项目

---
+++ base/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/27 9:32
+# @Author  : huangkaiding
---
+++ base/practice1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/27 9:32
+# @Author  : huangkaiding
+
+
+if __name__ == '__main__':
+    for value in range(1, 5):
+        print(value)
