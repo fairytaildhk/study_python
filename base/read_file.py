15fbb7a6abdcc56f14e6bd390e59385f173183f2
fairytail <272087260@qq.com>
2018-12-15 16:51:45 +0800

    读取文件

---
+++ base/read_file.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/12/15 16:43
+# @Author  : huangkaiding
+
+if __name__ == '__main__':
+    with open('file1.txt') as file:
+        contents = file.read()
+        print(contents)
+        print('-------------------------')
+    with open('file1.txt') as file:
+        for line in file:
+            print(line.rstrip())
