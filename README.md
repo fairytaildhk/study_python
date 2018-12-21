d6803abcdb331e1e39ee5392dda4d9408e8cef94
fairytail <272087260@qq.com>
2018-12-21 19:35:16 +0800

    Django项目更新

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # study_python
+superuser
+admin
+testtest123
--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 我的应用程序
+    'learning_logs',
 ]
 
 MIDDLEWARE = [
