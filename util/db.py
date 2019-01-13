cd4b23660e7796628fc1d67fc48f9497430b0009
fairytaildhk <272087260@qq.com>
2019-01-13 22:41:13 +0800

    豆瓣电影top250爬去并落库

---
+++ util/db.py
@@ -0,0 +1,52 @@
+#coding=utf-8
+import pymysql
+
+from util.db_config import db_dic
+
+
+def db_query(db, sql):
+    # 连接数据库
+    global db_info
+    db_info = db_dic[db]
+    conn = pymysql.connect(host=db_info[0], user=db_info[1],
+                           password=db_info[2], db=db_info[3], port=db_info[4])  # db：库名
+    try:
+        # 设置游标类型，默认游标类型为元祖形式
+        # 将游标类型设置为字典形式
+        cur = conn.cursor(cursor=pymysql.cursors.DictCursor)
+        cur.execute(sql)
+        # 将表中所有数据以字典形式输出
+        ret = cur.fetchall()
+        # print(ret)
+        # 提交
+        conn.commit()
+        # 关闭指针对象
+        cur.close()
+        return ret
+    finally:
+        # 关闭连接对象
+        conn.close()
+
+
+def db_update(db, sql):
+    # 连接数据库
+    global db_info
+    db_info = db_dic[db]
+    conn = pymysql.connect(host=db_info[0], user=db_info[1],
+                           password=db_info[2], db=db_info[3], port=db_info[4], charset='utf8')  # db：库名
+    try:
+        # 设置游标类型，默认游标类型为元祖形式
+        # 将游标类型设置为字典形式
+        cur = conn.cursor(cursor=pymysql.cursors.DictCursor)
+        cur.execute(sql)
+        conn.commit()
+        # 关闭指针对象
+        cur.close()
+    finally:
+        # 关闭连接对象
+        conn.close()
+
+
+if __name__ == '__main__':
+    sql = "SELECT * from douban_movie"
+    print(db_query('test', sql))
---
+++ util/db_config.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/11/17 14:31
+# @Author  : huangkaiding
+
+
+db_dic = {"test": ["localhost", "root", "123456", "test", 3306],
+          }
+
